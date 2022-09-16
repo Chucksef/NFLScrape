56c201e049825963fff6a21fe9a0d6d357113758
Chucksef <chucksef@gmail.com>
2022-09-16 13:02:28 -0600

    Added basic logic for scraping

--- NFLScrape.py
+++ NFLScrape.py
@@ -1,1 +1,7 @@
-print("Hello World")
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get("https://www.pro-football-reference.com/years/2022/games.htm").text
+soup = BeautifulSoup(html_text, 'lxml')
+
+print(source)
