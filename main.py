3709c36b72640c6d798d1f1d266c28a9b3289ee3
Chuck Crouse <Chucksef@gmail.com>
2022-09-27 14:49:00 -0600

    Added logic to auto-grab current season
    Edited main.py such that it always updates the database for the current season

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from scrapeToJSON import scrapeToJSON
 from processStats import processStats
 from populateFirebase import populateFirebase
+from datetime import date
 
 # Goals:
-# 1) COMPLETED -- Move data into type-separated folders
-# 2) COMPLETED -- Create year-specific translate files
-# 3) COMPLETED -- Move firebase credentials into data folder
-# 4) COMPLETED -- Add firebase info to .gitignore
-
-years = [2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022]
+# This is where my current to-do list will go...
 
 # MAIN CODE EXECUTION BELOW:
+today = date.today()
+month = today.month
+year = today.year
 
-for year in years:
-    scrapeToJSON(year)
-    processStats(year)
-    populateFirebase(year)
+# if it is not yet May, revert to previous year
+if (month < 5): year -= 1
+
+scrapeToJSON(year)
+processStats(year)
+populateFirebase(year)
