6156ea5689d4910d70be2381f4a37408c3cc06d7
Chuck Crouse <Chucksef@gmail.com>
2022-09-27 13:30:39 -0600

    Re-organized data; Re-worked Translate Files
    • Moved Data into type-separated folders under /data/
    • Created year-specific translate files also located under /data/

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from scrapeToJSON import scrapeToJSON
 from processStats import processStats
 from populateFirebase import populateFirebase
 
+# Goals:
+# 1) COMPLETED -- Move data into type-separated folders
+# 2) COMPLETED -- Create year-specific translate files
+# 3) Move firebase credentials into data folder
+# 4) Add firebase info to .gitignore
+
 # MAIN CODE EXECUTION BELOW:
 scrapeToJSON(2016)
 scrapeToJSON(2017)
